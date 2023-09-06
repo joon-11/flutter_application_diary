4384b7ad98feee4f06c5b23fbcf06aae99433b47
joon-11 <123048555+joon-11@users.noreply.github.com>
2023-09-06 17:12:50 +0900

    Add files via upload

--- lib/add_page.dart
+++ lib/add_page.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 class AddPage extends StatefulWidget {
@@ -9,9 +12,9 @@ class AddPage extends StatefulWidget {
 }
 
 class _AddPageState extends State<AddPage> {
-  String filePath = '';
+  String filePath = ' ';
 
-  List<TextEditingController> controllers = [
+  List<TextEditingController> contollers = [
     TextEditingController(),
     TextEditingController(),
   ];
@@ -20,6 +23,26 @@ class _AddPageState extends State<AddPage> {
   void initState() {
     super.initState();
     filePath = widget.filePath;
+  }
+
+  Future<bool> fileSave() async {
+    try {
+      File file = File(filePath);
+      List<dynamic> dataList = [];
+      var data = {'title': contollers[0].text, 'contents': contollers[1].text};
+
+      if (file.existsSync()) {
+        var fileContents = await file.readAsString();
+        dataList = jsonDecode(fileContents) as List<dynamic>;
+      }
+      dataList.add(data);
+      var jsonData = jsonEncode(dataList);
+      await file.writeAsString(jsonData);
+      return true;
+    } catch (e) {
+      print(e);
+      return false;
+    }
   }
 
   @override
@@ -31,38 +54,41 @@ class _AddPageState extends State<AddPage> {
       ),
       body: Form(
         child: Padding(
-          padding: const EdgeInsets.all(10.0),
+          padding: const EdgeInsets.all(8.0),
           child: Column(
             children: [
               TextFormField(
-                controller: controllers[0],
                 decoration: const InputDecoration(
                   border: OutlineInputBorder(),
                   label: Text('title'),
                 ),
               ),
               const SizedBox(
-                height: 50,
+                height: 30,
               ),
               Expanded(
                 child: TextFormField(
-                  controller: controllers[1],
+                  controller: contollers[0],
                   maxLength: 500,
                   maxLines: 10,
                   decoration: const InputDecoration(
                     border: OutlineInputBorder(),
-                    label: Text("content"),
+                    label: Text('contents'),
                   ),
                 ),
               ),
               ElevatedButton(
                 onPressed: () {
-                  var title = controllers[0].text;
-                  print(title);
-                  Navigator.pop(context, 'ok');
+                  var title = contollers[0].text;
+                  var result = fileSave();
+                  if (result == true) {
+                    Navigator.pop(context, 'OK');
+                  } else {
+                    print('저장실패');
+                  }
                 },
-                child: const Text("저장"),
-              )
+                child: const Text('저장'),
+              ),
             ],
           ),
         ),
---
+++ lib/main_page.dart
@@ -0,0 +1,69 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_application_diary/add_page.dart';
+import 'package:path_provider/path_provider.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  late Directory? directory;
+  String filePath = '';
+
+  @override
+  void initState() {
+    // TODO: implement initStat
+    getPath();
+  }
+
+  Future<void> getPath() async {
+    directory = await getApplicationCacheDirectory();
+    if (directory != null) {
+      var fileName = 'diray json';
+      filePath = '${directory!.path}/$fileName';
+      print(filePath);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Main(),
+    );
+  }
+}
+
+class Main extends StatefulWidget {
+  const Main({
+    super.key,
+  });
+
+  @override
+  State<Main> createState() => _MainState();
+}
+
+class _MainState extends State<Main> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const Text('hello'),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          var result = await Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => AddPage(filePath: 'temp'),
+            ),
+          );
+          print(result);
+        },
+        child: const Icon(Icons.apple),
+      ),
+    );
+  }
+}
